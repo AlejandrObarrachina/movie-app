d17270e9d394bd134a92d1988defdf1cd56a90a9
AlejandrObarrachina <alejandrobrokenh@gmail.com>
2022-08-16 18:01:56 +0200

    creation of microservice and API

---
+++ movie-catalog-service/src/main/java/com/springboottest/moviecatalogservice/models/UserRating.java
@@ -0,0 +1,21 @@
+package com.springboottest.moviecatalogservice.models;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UserRating {
+    private List<Rating> userRating;
+
+    public List<Rating> getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(List<Rating> userRating) {
+        this.userRating = userRating;
+    }
+
+    public UserRating GetUserRating() {
+
+        return null;
+    }
+}
